0bdb9de7f540d1a257bcc8ee821751331578a953
zyansuh <58452573+zyansuh@users.noreply.github.com>
2025-04-19 21:23:20 +0900

    feat: Todo 타입 정의

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import './globals.css'
 import { ReactNode } from 'react'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from '@/lib/queryClient'
+import { queryClient } from '@/lib/queryClients.ts'
 
 export const metadata = {
   title: 'Todo App',
